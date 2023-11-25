dc168a4de7901ee3c635ea7520cb2e3103cf1d38
kammi1a <150505313+kammi1a@users.noreply.github.com>
2023-11-25 23:28:15 +0600

    Update F.cpp

---
+++ F.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    int arr[1000];
+    int n;
+    cin >> n:
+    
+    for (int i=0; i<n; i++) 
+    cin >> arr[i];
+    int mx = arr[0];
+    int mnindex=0;
+    
+    for(int i=1; i<n; i++)
+{
+if( arr[i] >= mx) {
+    mx = arr[i];
+    mindex = i;
+}
+}
+
+mx = arr[mnindex];
+arr[mnindex]=arr[n-1];
+arr [n-1]=mx;
+
+for (int i=0; i<n; i++)
+cout << arr[i] <<" ";
+return 0;
+}
